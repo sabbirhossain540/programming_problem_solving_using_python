0849dd9d880d4eecc07495a03f853584ee6f2dee
Md Sabbir Hossain <sabbir@promiti.com>
2020-01-21 14:03:32 +0600

    Day 2

---
+++ Basic Problem/fibonacci_series.py
@@ -0,0 +1,13 @@
+
+def fiboNo(n):
+    t1=0
+    t2 = 1
+    nextTerm =0
+    for i in range(0,n):
+        print("{}  ".format(t1))
+        nextTerm = t1+t2
+        t1 = t2
+        t2 = nextTerm
+
+num = int(input("Enter a number: "))
+fiboNo(num)
