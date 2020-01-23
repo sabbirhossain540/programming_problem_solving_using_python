1079502aefa0318676e2e20866ac62bd8eff4153
Md Sabbir Hossain <sabbir@promiti.com>
2020-01-23 15:48:16 +0600

    Day 4

---
+++ Basic Problem/palindrome_problem.py
@@ -0,0 +1,14 @@
+num = int(input("enter a number: "))
+temp = num
+sum = 0
+
+while num > 0:
+    rev = num % 10
+    sum = sum * 10 + rev
+    num = num // 10
+
+if temp == sum:
+    print("Its a Palindrom Number")
+else:
+    print("Is not a Palindrome Number")
+    print(sum)
