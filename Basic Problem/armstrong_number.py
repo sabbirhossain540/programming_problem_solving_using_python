5a76e5e285ae6dab4590407458bf6051e8e70bbe
Md Sabbir Hossain <sabbir@promiti.com>
2020-01-23 15:25:57 +0600

    Day 4

---
+++ Basic Problem/armstrong_number.py
@@ -0,0 +1,14 @@
+
+num = int(input("Enter a Number: "))
+fix = num
+sum = 0
+while num > 0:
+    temp = num % 10
+    sum = sum + (temp * temp * temp)
+    num = num // 10
+
+
+if fix == sum:
+    print("{} is a Armstrong Number.".format(fix))
+else:
+    print("{} is not a Armstrong Number.".format(fix))
