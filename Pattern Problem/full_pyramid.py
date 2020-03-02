f7f22915732b9aa833d59a917a60264721380a3c
Md Sabbir Hossain <m.sabbirhossain308@gmail.com>
2020-03-02 19:20:33 +0600

    02-03-2020

---
+++ Pattern Problem/full_pyramid.py
@@ -0,0 +1,18 @@
+
+a = int(input("Enter Your Pyramid Size:"))
+
+k = 2*a - 1
+
+
+for i in range(0, a):
+    
+    for j in range(0, k):
+        print(end = " ")
+    
+    k = k-1
+
+    for j in range(0, i+1):
+        print("* ", end="")
+    
+    print("\r")
+    
